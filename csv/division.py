766efb22cfe0674936e6d55147a775d2c4bd111b
mesadeordenador <pedro.acoll@gmail.com>
2019-11-16 14:18:38 +0100

    Dataset Dividida
    Cada recorrido tiene asociado una carpeta que tiene por una parte un csv con los puntos y por otra otro csv con  las distancias

---
+++ csv/division.py
@@ -0,0 +1,29 @@
+import os
+import csv
+
+
+directory='/Users/pedroguillermarnanzcoll/Documents/AI/Arquitectura/csv/1260'
+
+
+for filename in os.listdir(directory):
+	if filename.endswith(".csv"):
+		edificio=filename[:-4]
+		print(edificio)
+		carpeta=os.mkdir(directory+'/'+edificio)
+		
+		with open(directory+'/'+filename,"r+") as file:
+			edificio
+			csv_in = csv.reader(file, delimiter=",")
+			print(directory+'/'+edificio+'/'+edificio+'_puntos.csv')
+			
+			with open(directory+'/'+edificio+'/'+edificio+'_puntos.csv',"w") as file_puntos, open(directory+'/'+edificio+'/'+edificio+'_direcciones.csv',"w") as file_direcciones: 
+				csv_puntos = csv.writer(file_puntos, delimiter=",")
+				csv_direc=csv.writer(file_direcciones, delimiter=",")
+				for row in csv_in:
+					csv_puntos.writerow((row[0],row[1],row[2],row[4]))
+					csv_direc.writerow(row[5:])
+
+		continue
+	else:
+		continue
+
